32947bbc26b1fe22bc8f384e33a482aa2ddf4f9c
Jan Bayer <enderjanyt@gmail.com>
2023-01-23 17:40:42 +0100

    Add jsons

--- src/SessionDesigner.java
+++ src/SessionDesigner.java
@@ -1,5 +1,17 @@
 public class SessionDesigner {
+
+  public static int desiredDuration = 0;
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    if (args.length != 3) {
+      System.err.println("Usage: java SessionDesigner <seconds> /path/to/yoga_poses.json /path/to/yoga_transitions.json");
+      return;
+    }
+    if (args[0].matches("[0-9]+")) {
+      desiredDuration = Integer.parseInt(args[0]);
+    } else {
+      System.err.println("Usage: java SessionDesigner <seconds> /path/to/yoga_poses.json /path/to/yoga_transitions.json");
+      return;
+    }
+    System.out.println(desiredDuration);
   }
 }
